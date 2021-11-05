0eaf8f51ca9ea9b6fd3b1de4c1351e69b506740a
fei <fei-au@qq.com>
2021-11-05 13:40:04 +0800

    modify refs files

--- src/app03-refs/App.jsx
+++ src/app03-refs/App.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.myRef = React.createRef();
+    }
 
+    componentDidMount(){
+        console.log('myRef', this.myRef);
+        console.log('myRef', this.myRef.current);
+    }
     render(){
         return (
-            <div>
-
+            <div ref={this.myRef}>
+                ......
             </div>
         );
     }
