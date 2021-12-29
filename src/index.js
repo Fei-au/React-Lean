e873a9f56e7acc66cc1a7f7c99a9ca1f108514a2
fei <yafeiliu@163.com>
2021-12-29 20:29:24 +0800

    update git route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link} from 'react-router';
 import './index.css';
 import App from './app05-smallgame/App';
 // import reportWebVitals from './reportWebVitals';
